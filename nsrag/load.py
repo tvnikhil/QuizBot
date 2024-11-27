a880c16f808976cc2829b7e74fc7bb58cfc3af9f
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-11-27 10:25:49 -0600

    minor changes

--- nsrag/load.py
+++ nsrag/load.py
@@ -1,5 +1,6 @@
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
 from langchain.vectorstores.chroma import Chroma
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 import warnings
@@ -8,16 +9,50 @@ warnings.filterwarnings("ignore")
 CHROMA_PATH = "chroma"
 DATA_PATH = "data"
 
-def split_documents(documents):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=80)
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+    )
     return text_splitter.split_documents(documents)
 
-documents = PyPDFDirectoryLoader(DATA_PATH).load()
-chunks = split_documents(documents)
-db = Chroma(persist_directory=CHROMA_PATH, embedding_function=OllamaEmbeddings(model="nomic-embed-text"))
-for idx, chunk in enumerate(chunks):
-    source = chunk.metadata.get("source", "Unknown Source")
-    page = chunk.metadata.get("page", "Unknown Page")
-    chunk.metadata["page_info"] = f"{source}:{page}:{idx}"
-db.add_documents(chunks, ids=[chunk.metadata["page_info"] for chunk in chunks])
-db.persist()  # Save changes to disk
+def add_to_chroma(chunks: list[Document]):
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=OllamaEmbeddings(model="nomic-embed-text"))
+    chunks_with_ids = calculate_chunk_ids(chunks)
+    existing_ids = set(db.get()["ids"])
+    
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+    if len(new_chunks):
+        print(f"Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        db.persist() #save the db or changes to the disk
+    else:
+        print("No new documents to add")
+
+def calculate_chunk_ids(chunks):
+    last_page_id = None
+    current_chunk_index = 0
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+        chunk.metadata["id"] = chunk_id
+    return chunks
+
+if __name__ == "__main__":
+    # Loading documents
+    documents = PyPDFDirectoryLoader(DATA_PATH).load()
+    #Splitting documents
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
