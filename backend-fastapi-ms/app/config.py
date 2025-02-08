53c646777f6bf1a957e23d58e1cc61b2d75b7a40
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2025-02-07 22:43:30 -0600

    config

--- backend-fastapi-ms/app/config.py
+++ backend-fastapi-ms/app/config.py
@@ -5,8 +5,10 @@ origins = [
 # BASE_URL = "http://localhost:11434/v1" # local
 BASE_URL = "http://34.174.202.104:11434/v1" # gcp
 API_KEY = "ollama"
-CHROMA_PATH = "/Users/thanikella_nikhil/Projects-Courses/NS/QuizBot/backend-fastapi-ms/chroma/chroma1"
-DATA_PATH = "/Users/thanikella_nikhil/Projects-Courses/NS/QuizBot/backend-fastapi-ms/data/data1"
+# CHROMA_PATH = "/Users/thanikella_nikhil/Projects-Courses/NS/QuizBot/backend-fastapi-ms/chroma/chroma1"
+CHROMA_PATH = "/home/nikhilvenkata_t/QuizBot/backend-fastapi-ms/chroma/chroma1"
+# DATA_PATH = "/Users/thanikella_nikhil/Projects-Courses/NS/QuizBot/backend-fastapi-ms/data/data1"
+DATA_PATH = "/home/nikhilvenkata_t/QuizBot/backend-fastapi-ms/data/data1"
 
 # /home/nikhilvenkata_t/QuizBot/backend-fastapi-ms/chroma/chroma1
 # /home/nikhilvenkata_t/QuizBot/backend-fastapi-ms/data/data1
